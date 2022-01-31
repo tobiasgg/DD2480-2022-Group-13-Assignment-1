1f2c86779ab4de68cc757a0bd92109fcdaf8ce7f
Edwin So <edwinso@kth.se>
2022-01-31 23:23:01 +0100

    feat: implement FinalUnlockingVectorCalculator

--- src/main/java/decide/FinalUnlockingVectorCalculator.java
+++ src/main/java/decide/FinalUnlockingVectorCalculator.java
@@ -1,10 +1,31 @@
 package decide;
 
-public class FinalUnlockingVectorCalculator {
+public class FinalUnlockingVectorCalculator implements Decide.FinalUnlockingVectorCalculator {
 
     public boolean[] calculate(boolean[][] pum, boolean[] puv) {
-        // TODO Auto-generated method stub
-        return null;
+        boolean[] fuv = new boolean[15];
+
+        for (int i = 0; i < 15; i++) {
+            if (puv[i]) {
+                boolean flag = true;
+
+                for (int j = 0; j < 15; j++) {
+                    if (i == j) {
+                        continue;
+                    }
+
+                    flag = flag && pum[i][j];
+                }
+
+                // if all elements in PUM row i are true
+                fuv[i] = flag;
+            } else {
+                // FUV[i] should be set to true if PUV[i] is false
+                fuv[i] = true;
+            }
+        }
+
+        return fuv;
     }
 
 }
